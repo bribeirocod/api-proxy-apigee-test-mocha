672a6c06fe1a17e5eadec0c9d181b02efa35d0a7
Bruno Henrique Brito Ribeiro <ribeirob@N087849.local>
2020-11-10 18:16:21 -0300

    change return method

--- apiproxy/test/test_api-ip.js
+++ apiproxy/test/test_api-ip.js
@@ -1,48 +1,43 @@
 var expect = require('expect.js');
 var sinon = require('sinon');
+var request = require('request')
 
 
-const baseUrl = 'https://brunohbr123-eval-test.apigee.net/';
-
-
-GLOBAL.httpClient = {
-	send: function(s) {
-		
-	}
-};
-
-GLOBAL.Request = function(s) {};
-
-GLOBAL.responseJsonObject = { ip: '35.224.230.242'}
-
-var httpClientSendMethod;
-var requestConstructor;
+var baseUrl = 'https://brunohbr123-eval-test.apigee.net';
+var responseDataObject, responseObject;
 
 // This method will execute before every it() method in the test
 // we are stubbing all Apigee objects and the methods we need here
-beforeEach(function () {
-	
-	requestConstructor = sinon.spy(GLOBAL, 'Request');
-	httpClientSendMethod = sinon.stub(httpClient, 'send');
-});
-
-// restore all stubbed methods back to their original implementation
-afterEach(function() {
-	requestConstructor.restore();
-});
-
-// this is the Loggly test feature here
-describe('feature: get ip api', function() {
-	it('should get the ip address', function() {
-	
-		var requestConstructorArgs = requestConstructor.args[0];
-		expect(requestConstructorArgs[0]).to.equal('https://brunohbr123-eval-test.apigee.net/firstproxy');		
-		expect(requestConstructorArgs[1]).to.equal('GET');		
-		expect(requestConstructorArgs[2]['Content-Type']).to.equal('application/json');		
-		var userPayloadObject = JSON.parse(requestConstructorArgs[3]);
-		expect(userPayloadObject.ip).to.equal('35.224.230.242');		
-
+describe('when stubbed', () => {
+	beforeEach(() => {
+	   responseObject = {
+		statusCode: 200,
+		headers: {
+		  'content-type': 'application/json'
+		}
+	  };
+		responseBody = {
+		ip: '35.224.230.242'
+	  };
+	  this.get = sinon.stub(request, 'get');
 	});
-
-
-});
+  
+	afterEach(() => {
+	  request.get.restore();
+	});
+	describe('GET /firstproxy', () => {
+	  it('should return ip ', (done) => {
+		this.get.yields(null, responseObject, JSON.stringify(responseBody));
+		request.get(`${baseUrl}/firstproxy`, (err, res, body) => {
+		  // there should be a 200 status code
+		  expect(res.statusCode).to.equal(200);
+		  // the response should be JSON
+		  expect(res.headers['content-type']).to.equal('application/json')
+		  // parse response body
+		  body = JSON.parse(body);
+		  expect(body.ip).to.equal('35.224.230.242');
+		  done();
+		});
+	  });
+	});
+  });
