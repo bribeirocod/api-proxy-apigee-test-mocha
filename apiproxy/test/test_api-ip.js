caf4e1ba59afbd06f3a56e702a27f7ae502fb3f9
Bruno Henrique Brito Ribeiro <ribeirob@N087849.local>
2020-11-10 16:35:36 -0300

    ajuste stup

--- apiproxy/test/test_api-ip.js
+++ apiproxy/test/test_api-ip.js
@@ -2,44 +2,40 @@ var expect = require('expect.js');
 var sinon = require('sinon');
 
 
+const baseUrl = 'https://brunohbr123-eval-test.apigee.net/';
 
-GLOBAL.context = {
-	getVariable: function(s) {},
-	setVariable: function(s) {}
-};
 
 GLOBAL.httpClient = {
-	send: function(s) {}
+	send: function(s) {
+		
+	}
 };
 
 GLOBAL.Request = function(s) {};
 
-var contextGetVariableMethod, contextSetVariableMethod;
+GLOBAL.responseJsonObject = { ip: '35.224.230.242'}
+
 var httpClientSendMethod;
 var requestConstructor;
 
 // This method will execute before every it() method in the test
 // we are stubbing all Apigee objects and the methods we need here
 beforeEach(function () {
-	contextGetVariableMethod = sinon.stub(context, 'getVariable');
-	contextSetVariableMethod = sinon.stub(context, 'setVariable');
+	
 	requestConstructor = sinon.spy(GLOBAL, 'Request');
 	httpClientSendMethod = sinon.stub(httpClient, 'send');
 });
 
 // restore all stubbed methods back to their original implementation
 afterEach(function() {
-	contextGetVariableMethod.restore();
-	contextSetVariableMethod.restore();
 	requestConstructor.restore();
-	httpClientSendMethod.restore();
 });
 
 // this is the Loggly test feature here
-describe('feature: get ip firt api', function() {
+describe('feature: get ip api', function() {
 	it('should get the ip address', function() {
 	
-		var requestConstructorArgs = requestConstructor;
+		var requestConstructorArgs = requestConstructor.args[0];
 		expect(requestConstructorArgs[0]).to.equal('https://brunohbr123-eval-test.apigee.net/firstproxy');		
 		expect(requestConstructorArgs[1]).to.equal('GET');		
 		expect(requestConstructorArgs[2]['Content-Type']).to.equal('application/json');		
@@ -47,4 +43,6 @@ describe('feature: get ip firt api', function() {
 		expect(userPayloadObject.ip).to.equal('35.224.230.242');		
 
 	});
+
+
 });
